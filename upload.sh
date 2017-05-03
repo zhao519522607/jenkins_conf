c325476b2b0683ab05b902666cde797d172d3a59
Figure <zhao519522607@users.noreply.github.com>
2017-05-03 16:48:06 +0800

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+PROJECT=""
+LN_name=""
+
+update() {
+        #保留最近3次的更新目录
+        ls -lt /data/web/$PROJECT |tail -n +2 |awk 'BEGIN {FS=" "} NR > 3 {print $NF}' |xargs -i rm -rf /data/web/$PROJECT/{}
+        #解压
+        tar zxf /data/upload/$PROJECT.tgz -C /data/web/$PROJECT/
+        name=$PROJECT-$(date +"%F-%H%M")
+	find /data/web/$PROJECT/$PROJECT -type d -name ".svn" -exec rm -rf {} \; > /dev/null 2>&1
+        mv /data/web/$PROJECT/$PROJECT /data/web/$PROJECT/$name
+        if [ -h "/data/www/$LN_name" ];then
+                rm -rf /data/www/$LN_name
+                ln -s /data/web/$PROJECT/$name /data/www/$LN_name
+        else
+                ln -s /data/web/$PROJECT/$name /data/www/$LN_name
+        fi
+}
+
+rollback() {
+        dir_ver=`ls -lt /data/web/$PROJECT |tail -n +2 |awk -v arg=$roll_v 'BEGIN {FS=" "} NR==arg {print $NF}'`
+        if [ -h "/data/www/$LN_name" ];then
+                rm -rf /data/www/$LN_name
+                ln -s /data/web/$PROJECT/$dir_ver /data/www/$LN_name
+        else
+                ln -s /data/web/$PROJECT/$dir_ver /data/www/$LN_name
+        fi
+
+}
+
+case $1 in
+        update)
+                update
+                ;;
+        rollback)
+                roll_v=$2
+                rollback
+                ;;
+esac
